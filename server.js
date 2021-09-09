724a0ed2173bff1ece077d8cd6be26d8d143a495
kendrickPC <kennethpchang@gmail.com>
2021-09-09 15:23:05 -0700

    server.js file set up

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser')
 
 module.exports = app;
 
@@ -10,13 +9,19 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+var cors = require('cors');
+// Using the default settings
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+var bodyParser = require('body-parser');
+// parse application/json
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+// ...
+app.use('/api', apiRouter);
 
 
 // This conditional is here for testing purposes:
