be28b6bb6cc6a69202594da75405ce1a4a4a8a67
kendrickPC <kennethpchang@gmail.com>
2021-09-09 16:27:18 -0700

    server/api.js router structure set up

--- server/api.js
+++ server/api.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+// setting up router structure
+// routes required are minions, ideas, and meetings
+// '/api/minions'
+// '/api/ideas'
+// '/api/meetings'
 
+const minionsRouter = require('/minions', minionsRouter);
+const ideasRouter = require('/ideas', ideasRouter);
+const meetingsRouter = require('/meetings', meetingsRouter);
+
+apiRouter.use('/minions', minionsRouter);
+apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 module.exports = apiRouter;
